f04d8b192666ce5f02c4838f6d12ae43fabbaad0
swipe <pdbeard@indiana.edu>
2020-02-28 15:44:47 +0000

    Add Users and user profiles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,20 +9,21 @@
 
         <div class="col-9 pt-5">
             <div>
-                <h1>freeCodeCamp</h1>
+                <h1>{{$user->username}}</h1>
             </div>
-                <div class="d-flex">
+
+            <div class="d-flex">
                 <div class="pr-4"><strong>145 </strong>posts </div>
                 <div class="pr-4"><strong>24k </strong>followers </div>
                 <div class="pr-4"><strong>224 </strong>following </div>
             </div>
 
-            <div class="pt-4 font-weight-bold">freeCodecamp.org</div>
-            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
-            <div> <a href="#">www.freecodecamp.org</a></div>
-
+            <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
+            <div>{{$user->profile->description}}</div>
+            <div> <a href="#">{{$user->profile->url ?? 'N/A'}}</a></div>
         </div>
     </div>
+
     <div class="row pt-4">
         <div class="col-4">
             <img src="img/me.jpg" class="w-100" alt="">
@@ -33,8 +34,7 @@
         <div class="col-4">
             <img src="img/me.jpg" class="w-100" alt="">
         </div>
-
-
     </div>
+
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
