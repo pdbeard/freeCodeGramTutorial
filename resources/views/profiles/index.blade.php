bda7c58312fb72ea390ebdb728863f36511cd4e1
swipe <pdbeard@indiana.edu>
2020-04-09 14:24:49 +0100

    profile image change

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="/img/me.jpg" class="rounded-circle img-thumbnail" alt="">
+        <img src="/storage/{{$user->profile->image}}" class="rounded-circle img-thumbnail" alt="">
         </div>
 
         <div class="col-9 pt-5">
